c736b55f58a69f9d18c84085691919ec0c15ee74
rishabh-hub <f20191056@goa.bit-pilani.ac.in>
2020-07-09 21:16:51 +0530

    > Testing sucessful push

--- app/src/main/java/com/example/radyapp/MainActivity.java
+++ app/src/main/java/com/example/radyapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.radyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button hiBro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
