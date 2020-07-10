ba3a37a4a0c33ce2662bb2fc21c5360b59372226
Rishabh Singh <rishabh.1056@gmail.com>
2020-07-10 21:26:35 +0530

    > Fragments have been created

--- app/src/main/java/com/example/radyapp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/radyapp/Activities/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.radyapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.radyapp.Fragment.LoginFragment;
 import com.example.radyapp.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -13,6 +16,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loadFragment(new LoginFragment());
+    }
+
+    void loadFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.,fragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
 
     }
 }
