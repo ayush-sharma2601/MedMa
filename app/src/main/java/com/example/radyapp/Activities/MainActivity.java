34eff46ca64e57a6e2eb69482e9298441c20bf12
Rishabh Singh <rishabh.1056@gmail.com>
2020-07-10 21:10:10 +0530

    > Login activity has been created

--- app/src/main/java/com/example/radyapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/radyapp/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.radyapp;
+package com.example.radyapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.radyapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
