a4faf3481d30907ae1ca3818b70bcfd81d3cea35
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-07-12 17:37:47 +0530

    notices

---
+++ app/src/main/java/com/example/radyapp/StaffSide/ActivitiesS/StaffHome.java
@@ -0,0 +1,16 @@
+package com.example.radyapp.StaffSide.ActivitiesS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.radyapp.R;
+
+public class StaffHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_home);
+    }
+}
