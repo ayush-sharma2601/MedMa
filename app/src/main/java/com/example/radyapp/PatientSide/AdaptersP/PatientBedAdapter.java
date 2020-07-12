9bbe2f20dce45bcb6b49bfee41caccab79e14c58
Rishabh Singh <rishabh.1056@gmail.com>
2020-07-12 14:54:46 +0530

    > Bed Fragment working

--- app/src/main/java/com/example/radyapp/PatientSide/AdaptersP/PatientBedAdapter.java
+++ app/src/main/java/com/example/radyapp/PatientSide/AdaptersP/PatientBedAdapter.java
@@ -3,6 +3,9 @@ package com.example.radyapp.PatientSide.AdaptersP;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,11 +19,14 @@ public class PatientBedAdapter extends RecyclerView.Adapter<PatientBedAdapter.Pa
 
     ArrayList<PatientBedModel> patientBedModels ;
 
+    public PatientBedAdapter(ArrayList<PatientBedModel> patientBedModels) {
+        this.patientBedModels = patientBedModels;
+    }
 
     @NonNull
     @Override
     public PatientBedAdapterRvRH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new PatientBedAdapterRvRH(LayoutInflater.from(parent.getContext()).inflate(R.layout.patient_appointment_rv_layout,parent,false));
+        return new PatientBedAdapterRvRH(LayoutInflater.from(parent.getContext()).inflate(R.layout.patient_bed_rv_layout,parent,false));
     }
 
     @Override
@@ -35,12 +41,27 @@ public class PatientBedAdapter extends RecyclerView.Adapter<PatientBedAdapter.Pa
 
     public class PatientBedAdapterRvRH extends RecyclerView.ViewHolder
     {
+        TextView roomClass,roomOccupancy,roomVacancy,roomManager;
+        ImageView roomCallButton;
 
         public PatientBedAdapterRvRH(@NonNull View itemView) {
             super(itemView);
+
+            roomClass=itemView.findViewById(R.id.bed_class);
+            roomOccupancy=itemView.findViewById(R.id.beds_occupied);
+            roomVacancy=itemView.findViewById(R.id.beds_vacant);
+            roomManager=itemView.findViewById(R.id.bed_manager);
+            roomCallButton=itemView.findViewById(R.id.manager_bed_call_btn);
+
         }
 
         public void populateBeds(PatientBedModel patientBedModel) {
+
+            roomClass.setText(patientBedModel.getBedClass());
+            roomOccupancy.setText(patientBedModel.getOccupancy());
+            roomVacancy.setText(patientBedModel.getVacancy());
+            roomManager.setText(patientBedModel.getWardManager());
+            //set Intent on call button here
         }
     }
 }
--- app/src/main/java/com/example/radyapp/PatientSide/FragmentsP/PatientBedFragment.java
+++ app/src/main/java/com/example/radyapp/PatientSide/FragmentsP/PatientBedFragment.java
@@ -6,15 +6,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.radyapp.PatientSide.AdaptersP.PatientBedAdapter;
+import com.example.radyapp.PatientSide.ModelsP.PatientBedModel;
 import com.example.radyapp.R;
+
+import java.util.ArrayList;
 
 public class PatientBedFragment extends Fragment {
 
     View view;
     RecyclerView bedRv;
-
+    PatientBedAdapter patientBedAdapter;
+    ArrayList<PatientBedModel> patientBedModels;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,23 +46,34 @@ public class PatientBedFragment extends Fragment {
     }
 
     private void attachId() {
+        bedRv=view.findViewById(R.id.patient_bed_rv);
     }
 
     private void attachLayout() {
+        bedRv.setLayoutManager(new LinearLayoutManager(view.getContext(),
+                LinearLayoutManager.VERTICAL,false));
     }
 
     private void initArrayList() {
+        patientBedModels=new ArrayList<>(3);
     }
 
     private void addData() {
+        for (int i=0;i<10;i++)
+            patientBedModels.add(new PatientBedModel("Private","300"
+                    ,"80","Karen","9318458933"));
     }
 
     private void initAdapter() {
+
+        patientBedAdapter=new PatientBedAdapter(patientBedModels);
     }
 
     private void attachAdapter() {
+        bedRv.setAdapter(patientBedAdapter);
     }
 
     private void refreshAdapter() {
+        patientBedAdapter.notifyDataSetChanged();
     }
 }
